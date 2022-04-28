568397ff728fa3262a4683840011b9aa27721278
Prithvi Prakash <Sunny@Prithvis-MacBook-Pro-2.local>
2022-04-28 18:57:03 -0500

    cleaned up constrained filtering and info theory starter code

--- services.py
+++ services.py
@@ -1,5 +1,7 @@
 import random
 import collections
+import numpy as np
+import pandas as pd
 
 
 words = []
@@ -7,10 +9,8 @@ with open('data/words.txt') as f:
     for line in f:
         words.append(line.strip())
 
-answer = random.choice(words)
 
-
-def validate(guess):
+def internal_validate(answer, guess):
     colors = ['0', '0', '0', '0', '0']
     character_matches = dict(collections.Counter(list(answer)) & collections.Counter(list(guess)))
     for i in range(5):
@@ -28,3 +28,24 @@ def validate(guess):
                 character_matches[guess[i]] -= 1
 
     return "".join(colors)
+
+def validate(answer, guess):
+    colors = list(internal_validate(answer, guess))
+    flag = False
+    if all(x == '1' for x in colors):
+        flag = True
+    return flag, "".join(colors)
+
+def prepareWordDF():
+    with open("./data/words.txt") as f:
+        words = f.readlines()
+        
+    cols = []
+    for col in np.arange(5):
+        cols.append(f"c{col}")
+    df = pd.DataFrame(columns = cols)
+    for idx, word in enumerate(words):
+        print(f"{idx+1}/{len(words)}", end="\r")
+        df = pd.concat([df, pd.DataFrame([list(word.strip())], columns=cols)], ignore_index = True, axis = 0)
+    
+    return df
